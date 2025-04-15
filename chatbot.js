52a960d3ba8654c49f2772f9af476706dd3bad1b
Samuel Teles Oliveira <samuellindst@gmail.com>
2025-04-15 18:09:34 -0300

    Update da porta 4000 para a nova 3002

--- chatbot.js
+++ chatbot.js
@@ -3,7 +3,7 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3002;
 
 const client = new Client({
     authStrategy: new LocalAuth(),
